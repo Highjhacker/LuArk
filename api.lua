1bb4362f238bf5361fc855a6b936eee37ed4815d
Christopher Wang <dev@christopher.wang>
2018-04-23 22:20:11 -0700

    Add post method to api; allow for network configurations

--- api.lua
+++ api.lua
@@ -3,24 +3,44 @@ local requests = require('requests')
 
 local API = class('API')
 
-function API:initialize()
-  self.base_url = "https://api.arknode.net/"
+local ARK_MAINNET_URL = 'https://api.arknode.net/'
+local ARK_MAINNET_NETHASH = '6e84d08bd299ed97c212c886c98a57e36545c8f5d645ca7eeae63a8bd62d8988'
+local ARK_MAINNET_VERSION = '1.0.1'
+local ARK_MAINNET_PORT = 4001
+
+function API:initialize(base_url, nethash, version, port)
+  self.base_url = base_url or ARK_MAINNET_URL
+  self.headers = {
+    nethash=nethash or ARK_MAINNET_NETHASH,
+    version=version or ARK_MAINNET_VERSION,
+    port=port or ARK_MAINNET_PORT,
+    ['Content-Type']='application/json'
+  }
 end
 
 function API:get(endpoint, ...)
-
-  headers = {
-      ["nethash"] = '6e84d08bd299ed97c212c886c98a57e36545c8f5d645ca7eeae63a8bd62d8988',
-      ["version"] = '1.0.1',
-      ["port"] = '4001'
+  local response = requests.get{
+    url=self.base_url..endpoint,
+    headers=self.headers,
+    params=...
   }
-
-  local query_parameters = ...
-
-  response = requests.get{url=self.base_url..endpoint, headers=headers, params=query_parameters}
 
   if response.status_code == 200 then
     return response.text
+  else
+    print("Error while fetching the URL.")
+  end
+end
+
+function API:post(endpoint, ...)
+  local response = requests.post{
+    url=self.base_url..endpoint,
+    headers=self.headers,
+    data=...,
+  }
+
+  if response.status_code == 200 then
+    return response.json()
   else
     print("Error while fetching the URL.")
   end
